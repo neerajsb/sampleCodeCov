bf622deb9f436a0150a2899d8a3687d8ec718856
neerajsb <neerajsbadamikar@gmail.com>
2017-12-07 17:21:04 -0500

    Update checkInputs.m

--- checkInputs.m
+++ checkInputs.m
@@ -1,4 +1,4 @@
-    function checkInputs
+    function checkInputs(a,b,c)
         if ~isa(a,'numeric') || ~isa(b,'numeric') || ~isa(c,'numeric')
             error('quadraticSolver:InputMustBeNumeric', ...
                 'Coefficients must be numeric.')
