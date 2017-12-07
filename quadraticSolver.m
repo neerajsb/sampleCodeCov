9a908af1b45e28c082e3aadc1a6555e0164a83a3
neerajsb <neerajsbadamikar@gmail.com>
2017-12-07 17:21:24 -0500

    Update quadraticSolver.m

--- quadraticSolver.m
+++ quadraticSolver.m
@@ -2,7 +2,7 @@ function roots = quadraticSolver(a,b,c)
 % quadraticSolver returns solutions to the
 % quadratic equation a*x^2 + b*x + c = 0.
 
-checkInputs
+checkInputs(a,b,c)
 
 roots(1) = (-b + sqrt(b^2 - 4*a*c)) / (2*a);
 roots(2) = (-b - sqrt(b^2 - 4*a*c)) / (2*a);
